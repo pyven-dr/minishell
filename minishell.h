d27f48d872d9a06be9f3e9353c61209c6082263d
Simon Abitbol <sabitbol@z4r6p1.42lyon.fr>
2024-03-18 14:39:09 +0100

    Binary Tree

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: sabitbol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 14:02:22 by sabitbol          #+#    #+#             */
-/*   Updated: 2024/03/17 15:07:57 by sabitbol         ###   ########.fr       */
+/*   Updated: 2024/03/18 14:33:49 by sabitbol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 typedef struct s_tree
 {
+	struct s_tree	*parent;
 	struct s_tree	*right;
 	struct s_tree	*left;
 	struct s_cmd	*pipex;
@@ -23,9 +24,15 @@ typedef struct s_tree
 
 typedef struct s_cmd
 {
-	char	**cmd;
-	char	**fd;
+	char		**cmd;
+	struct s_file	*file;
 }	t_cmd;
+
+typedef struct s_file
+{
+	char			*fd;
+	struct e_chevron	chevron;
+}	t_file;
 
 typedef enum e_operand
 {
@@ -33,5 +40,11 @@ typedef enum e_operand
 	OR,
 	CMD,
 }	t_operand;
+
+typedef enum e_chevron
+{
+	SIMPLE,
+	DOUBLE,
+}	t_chevron;
 
 #endif
