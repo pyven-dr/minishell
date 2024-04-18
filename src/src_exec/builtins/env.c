73674b58bddf755f04d679f9c26f9deae008056c
Pablo Yven Drogrey <pyven-dr@student.42lyon.fr>
2024-04-18 18:55:16 +0200

    export : check args and add to vector need to change name if var already exist

--- src/src_exec/builtins/env.c
+++ src/src_exec/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: pyven-dr <pyven-dr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 20:46:16 by pyven-dr          #+#    #+#             */
-/*   Updated: 2024/04/18 18:01:01 by pyven-dr         ###   ########.fr       */
+/*   Updated: 2024/04/18 18:10:28 by pyven-dr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int	env(t_vector *env)
 	while (line != NULL)
 	{
 		if (line->equal == 1)
-			if (ft_printf(STDOUT_FILENO, "%s=%s\n", line->name, line->value) == -1)
+			if (ft_printf(STDOUT_FILENO, "%s=%s\n", \
+				line->name, line->value) == -1)
 				return (1);
 		i++;
 		line = get_elem_vector(env, i);
--- src/src_exec/builtins/export.c
+++ src/src_exec/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: pyven-dr <pyven-dr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 20:26:48 by pyven-dr          #+#    #+#             */
-/*   Updated: 2024/04/18 18:00:57 by pyven-dr         ###   ########.fr       */
+/*   Updated: 2024/04/18 18:52:55 by pyven-dr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,18 @@ int	display_env(t_vector *env)
 	line = get_elem_vector(env, i);
 	while (line != NULL)
 	{
-		if (ft_printf(STDOUT_FILENO, "declare -x %s=%s\n", line->name, line->value) == -1)
-			return(1);
+		if (line->equal == 1)
+		{
+			if (ft_printf(STDOUT_FILENO, "declare -x %s=\"%s\"\n", \
+				line->name, line->value) == -1)
+				return (1);
+		}
+		else
+		{
+			if (ft_printf(STDOUT_FILENO, "declare -x %s\n", \
+				line->name) == -1)
+				return (1);
+		}
 		i++;
 		line = get_elem_vector(env, i);
 	}
@@ -34,16 +44,45 @@ int	check_arg(char *arg)
 	int	i;
 
 	i = 1;
-	if (ft_isalpha(arg[i]) == 0 && arg[i] != '_')
+	if (ft_isalpha(arg[0]) == 0 && arg[0] != '_')
 		return (1);
 	while (arg[i] != '\0')
 	{
+		if (arg[i] == '=')
+			return (0);
 		if (ft_isalnum(arg[i]) == 0 && arg[i] != '_')
 			return (1);
-		if(arg[i] == '=')
-			return (0);
 		i++;
 	}
+	return (0);
+}
+
+int	add_env(char *arg, t_vector *env_vector)
+{
+	t_env	env;
+	int		i;
+
+	i = 0;
+	env.equal = 0;
+	while (arg[i] != '\0' && arg[i] != '=')
+		i++;
+	if (arg[i] == '=')
+	{
+		env.equal = 1;
+		arg[i] = '\0';
+	}
+	i++;
+	env.name = ft_strdup(arg);
+	if (env.name == NULL)
+		return (1);
+	env.value = ft_strdup(&arg[i]);
+	if (env.value == NULL)
+	{
+		free(env.name);
+		return (1);
+	}
+	arg[i - 1] = '=';
+	add_vector(env_vector, &env, free_env_line);
 	return (0);
 }
 
@@ -51,18 +90,29 @@ int	export(char **args, t_utils *utils)
 {
 	int	arg_nb;
 	int	i;
+	int	return_val;
 
-	i = 0;
+	i = 1;
+	return_val = 0;
 	arg_nb = nb_args(args);
 	if (arg_nb == 1)
+	{
 		if (display_env(utils->env_vector) == 1)
 			return (1);
+		return (0);
+	}
 	while (args[i] != NULL)
 	{
 		if (check_arg(args[i]) == 1)
 		{
+			return_val = 1;
+			if (ft_printf(2, "export: '%s': not a valid identifier\n", \
+				args[i]) == -1)
+				return (1);
 		}
+		else
+			add_env(args[i], utils->env_vector);
 		i++;
 	}
-	return (0);
+	return (return_val);
 }
