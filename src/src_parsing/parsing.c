6bd2d3196a2490eb224ff5c86d1ad5919a841766
Simon Abitbol <sabitbol@z4r2p7.42lyon.fr>
2024-05-03 19:48:14 +0200

    some norm

--- src/src_parsing/parsing.c
+++ src/src_parsing/parsing.c
@@ -6,15 +6,17 @@
 /*   By: sabitbol <sabitbol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 00:18:26 by sabitbol          #+#    #+#             */
-/*   Updated: 2024/05/03 14:20:24 by sabitbol         ###   ########.fr       */
+/*   Updated: 2024/05/03 19:47:31 by sabitbol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
+static bool	handle_parenthesis(t_parsing *pars, char **line);
+static bool	pars_line(t_parsing *pars, char **line);
+
 t_tree	*parse(char *line)
 {
-	t_tree		*saved_node;
 	t_parsing	pars;
 
 	pars.save = NULL;
@@ -25,44 +27,8 @@ t_tree	*parse(char *line)
 		return (NULL);
 	while (*line)
 	{
-		while (*line && is_whitespace(*line))
-			line++;
-		if (*line == '(' || *line == ')')
-		{
-			if (*line == '(')
-			{
-				if (!save_parenthesis(&pars, &line))
-					return (NULL);
-			}
-			else
-			{
-				if (!pars.save || !pars.tree || ((pars.tree->operand == AND \
-				|| pars.tree->operand == OR || pars.tree->operand == PIPE) && \
-				!pars.tree->right))
-					return (clean_continue(&pars, &line, 2));
-				saved_node = get_last_save(&pars);
-				if (saved_node)
-				{
-					pars.tree = saved_node;
-					pars.parenthes = 1;
-				}
-				else
-				{
-					while (pars.tree->parent)
-						pars.tree = pars.tree->parent;
-					pars.parenthes = 2;
-				}
-			}
-			line++;
-		}
-		else if (*line)
-		{
-			pars.operand = is_operand(&line);
-			while (*line && is_whitespace(*line))
-				line++;
-			if (!fill_tree(&pars, &line))
-				return (NULL);
-		}
+	if (!pars_line(&pars, &line))
+		return (NULL);
 	}
 	if (pars.save)
 		return (clean_continue(&pars, &line, 3));
@@ -75,6 +41,56 @@ t_tree	*parse(char *line)
 		while (pars.tree->parent)
 			pars.tree = pars.tree->parent;
 	return (pars.tree);
+}
+
+static bool	pars_line(t_parsing *pars, char **line)
+{
+
+	while (**line && is_whitespace(**line))
+		(*line)++;
+	if (**line == '(' || **line == ')')
+	{
+		if (!handle_parenthesis(pars, line))
+			return (false);
+	}
+	else if (**line)
+	{
+		pars->operand = is_operand(line);
+		while (**line && is_whitespace(**line))
+			(*line)++;
+		if (!fill_tree(pars, line))
+			return (false);
+	}
+	return (true);
+}
+
+static bool	handle_parenthesis(t_parsing *pars, char **line)
+{
+	t_tree		*saved_node;
+
+	if (**line == '(')
+	{
+		if (!save_parenthesis(pars, line))
+			return (false);
+	}
+	else
+	{
+		if (!pars->save || !pars->tree || ((pars->tree->operand == AND \
+		|| pars->tree->operand == OR || pars->tree->operand == PIPE) && \
+		!pars->tree->right))
+			return (clean_continue(pars, line, 2), false);
+		saved_node = get_last_save(pars);
+		if (saved_node)
+			return (pars->tree = saved_node, pars->parenthes = 1, \
+			(*line)++, true);
+		else
+		{
+			while (pars->tree->parent)
+				pars->tree = pars->tree->parent;
+			pars->parenthes = 2;
+		}
+	}
+	return ((*line)++, true);
 }
 
 char	*strdup_to_next_operand(char **line, t_parsing *pars)
