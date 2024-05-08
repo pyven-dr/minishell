e6aaf2c25e7a0da137121b5f5076c1c118ed48e3
Pablo Yven Drogrey <pyven-dr@student.42lyon.fr>
2024-05-08 15:45:42 +0200

    dont return when error during the main

--- src/src_parsing/tester.c
+++ src/src_parsing/tester.c
@@ -22,6 +22,8 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	utils.env_vector = new_vector(10, sizeof(t_env));
+	if (utils.env_vector == NULL)
+		return (1);
 	init_env(&utils, envp);
 	while (1)
 	{
@@ -33,13 +35,8 @@ int	main(int argc, char **argv, char **envp)
 		utils.root = tree;
 		utils.fds_vector = new_vector(10, sizeof(int));
 		if (utils.fds_vector == NULL)
-		{
-			free_tree(&tree);
-			close(STDIN_FILENO);
-			close(STDOUT_FILENO);
-			return (1);
-		}
-		if (tree != NULL)
+			change_exit_val(1, &utils);
+		if (tree != NULL && utils.fds_vector != NULL)
 		{
 			make_all_heredocs(tree);
 			check_id(exec(tree, &utils), &utils);
